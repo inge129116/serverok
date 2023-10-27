dea5c248fb775c6a4b0f67de7252f266a8e0cfd4
inge <s129116@ap.be>
2023-10-27 10:25:15 +0200

    add show choice

--- server_ok.py
+++ server_ok.py
@@ -1,2 +1,13 @@
+import sys
 print("hallo")
-optie = input("geef het getal van jou keuze: \n1.toevoegen1 \n2.verwijderen \n3.lijst tonen \n")
+optie = input("geef het getal van jou keuze: \n0. stoppen\n1. toevoegen1 \n2. verwijderen \n3. lijst tonen \n")
+print(f"je hebt gekozen voor optie {optie}")
+match optie:
+    case "1":
+        print("toevoegen")
+    case "2":
+        print("verwijderen")
+    case "3":
+        print("lijst tonen")
+    case "0":
+        sys.exit(0)
