b46b2ce24e413f4af6aa38b194e6852387ca7fac
inge <s129116@ap.be>
2023-10-27 10:36:36 +0200

    add working with command-line interface

--- server_ok.py
+++ server_ok.py
@@ -1,6 +1,9 @@
 import sys
-print("hallo")
-optie = input("geef het getal van jou keuze: \n0. stoppen\n1. toevoegen1 \n2. verwijderen \n3. lijst tonen \n")
+
+if len(sys.argv) >= 2:
+    optie = sys.argv[1]
+else:
+    optie = input("geef het getal van jou keuze: \n0. stoppen\n1. toevoegen1 \n2. verwijderen \n3. lijst tonen \n")
 print(f"je hebt gekozen voor optie {optie}")
 match optie:
     case "1":
