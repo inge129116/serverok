a9813230a635e7ffbcd3a4d0c89ecd94461ce5a7
inge <s129116@ap.be>
2023-10-27 11:01:01 +0200

    let the choice menu repeat

--- server_ok.py
+++ server_ok.py
@@ -10,17 +10,19 @@ def run_cli(args):
             print("lijst tonen")
 
 def run_int():
-    optie = input("geef het getal van jou keuze: \n0. stoppen\n1. toevoegen1 \n2. verwijderen \n3. lijst tonen \n")
-    print(f"je hebt gekozen voor optie {optie}")
-    match optie:
-        case "1":
-            print("toevoegen")
-        case "2":
-            print("verwijderen")
-        case "3":
-            print("lijst tonen")
-        case "0":
-            sys.exit(0)
+    
+    while(True):
+        optie = input("geef het getal van jou keuze: \n0. stoppen\n1. toevoegen \n2. verwijderen \n3. lijst tonen \n")
+        print(f"je hebt gekozen voor optie {optie}")
+        match optie:
+            case "1":
+                print("toevoegen")
+            case "2":
+                print("verwijderen")
+            case "3":
+                print("lijst tonen")
+            case "0":
+                sys.exit(0)
 
 if len(sys.argv) >= 2:
     run_cli(sys.argv[1:])
