25a26269ea32a90d327601ecdf3768246c49100b
inge <s129116@ap.be>
2023-10-27 10:56:36 +0200

    change working with command-line interface

--- server_ok.py
+++ server_ok.py
@@ -1,16 +1,28 @@
 import sys
 
-if len(sys.argv) >= 2:
-    optie = sys.argv[1]
-else:
+def run_cli(args):
+    match args[0]:
+        case "1":
+            print("toevoegen")
+        case "2":
+            print("verwijderen")
+        case "3":
+            print("lijst tonen")
+
+def run_int():
     optie = input("geef het getal van jou keuze: \n0. stoppen\n1. toevoegen1 \n2. verwijderen \n3. lijst tonen \n")
-print(f"je hebt gekozen voor optie {optie}")
-match optie:
-    case "1":
-        print("toevoegen")
-    case "2":
-        print("verwijderen")
-    case "3":
-        print("lijst tonen")
-    case "0":
-        sys.exit(0)
+    print(f"je hebt gekozen voor optie {optie}")
+    match optie:
+        case "1":
+            print("toevoegen")
+        case "2":
+            print("verwijderen")
+        case "3":
+            print("lijst tonen")
+        case "0":
+            sys.exit(0)
+
+if len(sys.argv) >= 2:
+    run_cli(sys.argv[1:])
+else:
+    run_int()
