7e561fdfe368ae6a9fd88101092d634de2b02ac8
inge <s129116@ap.be>
2023-11-07 08:46:51 +0100

    add checks file

--- server_ok.py
+++ server_ok.py
@@ -39,26 +39,32 @@ def run_int():
         optie = input("geef het getal van jou keuze: \n0. stoppen\n1. toevoegen \n2. verwijderen \n3. lijst tonen \n")
         print(f"je hebt gekozen voor optie {optie}")
         match optie:
-            case "1": #json overschrijven
+            case "1": 
                 
                 add_server(input("geef de url van de server: "))
                 
-            case "2": #json overschrijven
+            case "2": 
                 
                 delete_server(input("geef de nummer van de server die je wil verwijderen"))
                 
             case "3":
-                showlist() #enumerate bekijken
+                showlist() #enumerate
             case "0":
                 sys.exit(0)
 
- #moet uit json file met try expect en json niet in git servers.json in git ignore
+ #servers list moet uit json file met try expect en json niet in git servers.json in git ignore
 try:
     with open("servers.json","r") as f:
         servers = json.load(f)
 except:
         servers = []
         print("servers.json niet gevonden" )
+try:
+    with open("checks.json","r") as f:
+        checks = json.load(f)
+except:
+        checks = []
+        print("checks.json niet gevonden" )
 
 if len(sys.argv) >= 2:
     run_cli(sys.argv[1:])
